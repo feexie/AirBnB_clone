cc703635c945072a83f169def8afe05ee529fefe
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-07 20:40:35 +0100

    check if task 4 is good, to be disccused later.

--- models/base_model.py
+++ models/base_model.py
@@ -3,25 +3,29 @@
 
 import uuid
 from datetime import datetime
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
 
-    def __init__(self, id=None, created_at=None, updated_at=None, name=None, my_number=None):
+    def __init__(self, *args, **kwargs):
 
-        self.id = id or str(uuid.uuid4())
-
-        self.created_at = created_at or datetime.now()
-        self.updated_at = updated_at or datetime.now()
-
-        self.name = name
-        self.my_number = my_number
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key and key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time_format)
 
     def __str__(self):
         
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        updated_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         
